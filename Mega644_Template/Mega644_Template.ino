707efa1dc8a977a0c3594b9624c7860be3eaee65
James Maupin <james.maupin@att.net>
2016-05-05 09:33:57 -0500

    Moved files to individual folders as I figure out how to use git...

---
+++ Mega644_Template/Mega644_Template.ino
@@ -0,0 +1,72 @@
+ /*
+ * ProgramTitle.ino
+ *
+ * Program description goes here...
+ *
+ *
+ *                               ATMega644P Pin Usage....                  
+ *                                     +---\/---+
+ *                         (D 0) PB0  1|        |40  PA0 (AI0 / D31)                           
+ *                         (D 1) PB1  2|        |39  PA1 (AI1 / D30)                            
+ *                    INT2 (D 2) PB2  3|        |38  PA2 (AI2 / D29)                            
+ *                     PWM (D 3) PB3  4|        |37  PA3 (AI3 / D28)                            
+ *                     PWM (D 4) PB4  5|        |36  PA4 (AI4 / D27)                           
+ *                    MOSI (D 5) PB5  6|        |35  PA5 (AI5 / D26)                            
+ *                    MISO (D 6) PB6  7|        |34  PA6 (AI6 / D25)                            
+ *                     SCK (D 7) PB7  8|        |33  PA7 (AI7 / D24)                            
+ * Reset                         RST  9|        |32  AREF
+ * Power                         VCC 10|        |31  GND 
+ * Ground                        GND 11|        |30  AVCC
+ *                             XTAL2 12|        |29  PC7 (D 23)                                 
+ *                             XTAL1 13|        |28  PC6 (D 22)                                
+ * Serial in          RX0 (D 8)  PD0 14|        |27  PC5 (D 21) TDI                           
+ * Serial out         TX0 (D 9)  PD1 15|        |26  PC4 (D 20) TDO                            
+ *               INT0 RX1 (D 10) PD2 16|        |25  PC3 (D 19) TMS                             
+ *               INT1 TX1 (D 11) PD3 17|        |24  PC2 (D 18) TCK                             
+ *                    PWM (D 12) PD4 18|        |23  PC1 (D 17) SDA    I2C data
+ *                    PWM (D 13) PD5 19|        |22  PC0 (D 16) SCL    I2C clock
+ *                    PWM (D 14) PD6 20|        |21  PD7 (D 15) PWM                             
+ *                                     +--------+
+ * for a non-P chip, there is no RX1/TX1, they are ordinary I/O
+ */ 
+//
+//  -----[ Include Libraries ]------------------------------------------
+//
+//
+//  -----[ Defines ]----------------------------------------------------
+//
+//
+//  -----[ Constants ]--------------------------------------------------
+//
+//
+//  -----[ Analog I/O ]-------------------------------------------------
+//
+//
+//  -----[ Digital I/O -]-----------------------------------------------
+//
+//
+//  -----[ Variables ]--------------------------------------------------
+// 
+//
+//  -----[ library init: ]----------------------------------------------
+//
+//
+//  -----[ System Initilization ]---------------------------------------
+// 
+void setup () 
+{
+
+}
+//  -----[ Main Program Loop ]------------------------------------------
+// 
+void loop () 
+{
+
+}
+//
+//  -----[ Interrupt Service Routine ]----------------------------------
+//
+//
+//  -----[ Subroutines and Functions ]----------------------------------
+//
+
